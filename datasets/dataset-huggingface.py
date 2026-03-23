889c79d70b9f89cd224f548503129166891fafa7
vzaratearvizu <zaratearvizuv@gmail.com>
2026-03-22 22:48:13 -0400

    removed files not needed anymore due to not using previous dataset anymore

--- datasets/dataset-huggingface.py
+++
@@ -1,9 +0,0 @@
-import datasets
-
-ossia = datasets.load_dataset("ductai199x/open-set-synth-img-attribution", "arch") # or "gen"
-
-# see structure
-print(ossia)
-
-# access the first example
-print(ossia["train"][0])
